639d44468cdc0cb31bbbf5955bdc250d59949005
itsrafaangelo <rafael@brgweb.com.br>
2025-07-03 16:27:21 -0300

    Feat: Erro de caixa de ideia resolvido

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'content',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
